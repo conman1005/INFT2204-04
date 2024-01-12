30ed5f3223e268e26555d61f68709bc63259cbdf
conman1005 <connercullity@live.ca>
2024-01-11 20:44:36 -0500

    Added list counter and added to top of list
    Was able to follow instructions and make it work in code. Can add elements to the page.

--- week1/ICE1/scripts/list.js
+++ week1/ICE1/scripts/list.js
@@ -1,16 +1,18 @@
 // ADDING ITEMS TO START AND END OF LIST
 // Get the <ul> element
 
+var gList = document.getElementById("gList");
+
 // ADD NEW ITEM TO END OF LIST
 // Create element
 // Create text node
 // Add text node to element
 // Add element end of list
 
-var page = document.getElementById("page");
 var item = document.createElement("li");
-item.textContent = "Food";
-page.appendChild(item);
+var textNode = document.createTextNode("Food");
+item.appendChild(textNode);
+gList.appendChild(item);
 
 // ADD NEW ITEM START OF LIST
 // Create element
@@ -18,7 +20,10 @@ page.appendChild(item);
 // Add text node to element
 // Add element to list
 
-
+var item = document.createElement("li");
+var textNode = document.createTextNode("Pie");
+item.appendChild(textNode);
+gList.insertBefore(item, gList.firstChild);
 
 // All <li> elements
 
@@ -27,6 +32,13 @@ page.appendChild(item);
 // Loop through elements
 // Change class to cool
 
+var list = gList.children;
+var count = 0
+for (var i = 0; i < list.length; i++) {
+    list[i].setAttribute("class", "COOL");
+    count++;
+}
+
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
 // h2 element
@@ -34,3 +46,10 @@ page.appendChild(item);
 // No. of <li> elements
 // Content
 // Update h2 using innerHTML (not textContent) because it contains markup
+
+var page = document.getElementById("page");
+var item = document.createElement("h2");
+//var textNode = document.createTextNode("Count: " + count);
+//item.appendChild(textNode);
+item.innerHTML = ("Count: " + count);
+page.insertBefore(item, page.firstChild);
