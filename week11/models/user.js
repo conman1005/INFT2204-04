ce1b39876c4e5a5f6e7342a96a0bbe1d571da7f5
conman1005 <connercullity@live.ca>
2024-03-28 22:04:30 -0400

    Removed password

--- week11/models/user.js
+++ week11/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://conman1005:Cjccjc12!@mydb.pp82h84.mongodb.net/College');
+mongoose.connect('mongodb+srv://conman1005:********@mydb.pp82h84.mongodb.net/College');
 
 // mongoose.set('useCreateIndex', true);
 
